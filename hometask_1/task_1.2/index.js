685182baf24e4ee52842f985bc71dc77d5acda9c
shanaeva <ellka.sh@ya.ru>
2019-12-07 17:07:42 +0400

    task_3 Replacement of pipe with pipeline

--- hometask_1/task_1.2/index.js
+++ hometask_1/task_1.2/index.js
@@ -1,18 +1,19 @@
 import fs from 'fs';
 import csv from 'csvtojson';
+import {pipeline} from 'stream';
 
 const csvFilePath = './csv/data.csv';
 const txtFilePath = './csv/data.txt';
 
-const readStream = fs.createReadStream(csvFilePath);
-const writeStream = fs.createWriteStream(txtFilePath);
-
-readStream.pipe(csv({
-    noheader: false,
-    headers: ['book', 'author', 'amount', 'price'],
-    includeColumns: /book|author|price/,
-    colParser: {"price" : "number"}
-})
-    .on('error', (err) => console.assert(err)))
-
-    .pipe(writeStream);
+pipeline (
+    fs.createReadStream(csvFilePath),
+    csv({
+        noheader: false,
+        headers: ['book', 'author', 'amount', 'price'],
+        includeColumns: /book|author|price/,
+        colParser: {"price" : "number"}
+    })
+    .on('error', (err) => console.assert(err)),
+    fs.createWriteStream(txtFilePath),
+    (err) => err && console.assert(err),
+)
