dab42945543c024fe14d7e76a825148890e4c106
shanaeva <ellka.sh@ya.ru>
2020-01-25 12:02:27 +0400

    hometask_3 Added postgres

--- hometask_2/index.ts
+++ hometask_2/index.ts
@@ -1,9 +1,26 @@
 import express from 'express';
+import { Sequelize } from 'sequelize';
 import { router } from './controller';
-export const app = express();
 
-const port = process.env.PORT || 3000;
-app.use(express.json());
-app.use('/', router);
+export const sequelize = new Sequelize('users', 'admin', 'admin', {
+    host: 'localhost',
+    dialect: 'postgres',
+    port: 5432,
+});
 
-app.listen(port, () => console.log('started'));
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch((err) => {
+        console.error('Unable to connect to the database:', err);
+    });
+
+// export const app = express();
+
+// const port = process.env.PORT || 3000;
+// app.use(express.json());
+// app.use('/', router);
+
+// app.listen(port, () => console.log('started'));
